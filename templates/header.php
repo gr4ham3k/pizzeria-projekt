067c5575d7d8e347e2c13f3adf5eb1a1b4f0bae6
gr4ham3k <jakubczar04@gmail.com>
2025-12-05 13:50:52 +0100

    szybkie style z czata xd

--- templates/header.php
+++ templates/header.php
@@ -19,28 +19,31 @@
 <body>
     <header>
     <div id="header-div">
-        <p>Pizzeria</p>
-        <div class="user-section">
-            <?php if(isset($_SESSION['user_email'])): ?>
-                <span><?php echo $_SESSION['user_email']; ?></span>
-                <a href='?page=logout'>Wyloguj się</a>
-                <a href='?page=cart'>Koszyk</a>
-                <?php
-                    $userData = $user->get_user_id($_SESSION['user_email']);
-                    if($userData['rola'] == 'uzytkownik') {
-                        echo "<a href='?page=user-status'>Status</a>";
-                    }
-                    if($userData['rola'] == 'admin') {
-                        echo "<a href='?page=admin-panel'>Panel admina</a>";
-                    }
-                ?>
-            <?php else: ?>
-                <a href='?page=login'>Logowanie</a>
-                <a href='?page=register'>Rejestracja</a>
-            <?php endif; ?>
-            <a href='?page=main'>Strona główna</a>
+        <div id="header-div-inner">
+            <p>Pizzeria</p>
+            <div class="user-section">
+                <?php if(isset($_SESSION['user_email'])): ?>
+                    <span><?php echo $_SESSION['user_email']; ?></span>
+                    <a href='?page=logout'>Wyloguj się</a>
+                    <a href='?page=cart'>Koszyk</a>
+                    <?php
+                        $userData = $user->get_user_id($_SESSION['user_email']);
+                        if($userData['rola'] == 'uzytkownik') {
+                            echo "<a href='?page=user-status'>Status</a>";
+                        }
+                        if($userData['rola'] == 'admin') {
+                            echo "<a href='?page=admin-panel'>Panel admina</a>";
+                        }
+                    ?>
+                <?php else: ?>
+                    <a href='?page=login'>Logowanie</a>
+                    <a href='?page=register'>Rejestracja</a>
+                <?php endif; ?>
+                <a href='?page=main'>Strona główna</a>
+            </div>
         </div>
     </div>
     </header>
+
 
     <main>
